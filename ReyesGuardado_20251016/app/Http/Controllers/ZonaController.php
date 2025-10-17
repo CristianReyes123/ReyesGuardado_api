94876ecbe6ddadd35b71b9c6f996ea3d450ed9e1
Cristian <ia.rg100321@ufg.edu.sv>
2025-10-16 20:03:03 -0600

    subiendo el trabajo

---
+++ ReyesGuardado_20251016/app/Http/Controllers/ZonaController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ZonaController extends Controller
+{
+    //
+
+    public function obtenerZonas()
+    {
+        $Zona = new Zona();
+        $valores = $Zona::all();
+        $respuesta = 
+        [
+            "success" => true,
+            "msg" => "Valores devueltos por el EndPoint",
+            "data" => $valores,
+            "error" => "",
+            "total" => sizeof($valores)
+        ];
+        return respone()->json($respuesta,200);
+    }
+
+
+
+    public function obtenerZona($idzona)
+    {
+
+        $Zona = new Zona();
+        $valores = $Zona->where('id_departamento', $idzona)->get();
+        $respuesta= [
+            "success" => true,
+            "msg" => "Valores devueltos por el EndPoint",
+            "data" => $valores,
+            "error" => "",
+            "total" => sizeof($valores)
+        ];
+
+        return respone()->json($respuesta,200);
+    }
+}
