d5ec61bdcdaf136b0e4b3f0c528f425907572f4f
Cristian <cristian>
2025-10-25 21:24:41 -0600

    actualizando la actividad subida

--- ReyesGuardado_20251016/routes/api.php
+++ ReyesGuardado_20251016/routes/api.php
@@ -3,8 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ZonaController;
+use App\Http\controllers\RegisterController;
 
-Route::get('/user', function (Request $request) {
+Route::get('/user', function (Request $request) 
+{
     return $request->user();
 })->middleware('auth:sanctum');
 
@@ -14,3 +16,8 @@ Route::get('/zona/{idzona}',[ZonaController::class,'obtenerZona']); //SINGULAR
 Route::get('/zonaspais/{idpais}',[ZonaController::class,'obtenerZonaPais']); //
 Route::post('/nuevazona',[ZonaController::class,'crearZona']); 
 
+Route::controller(RegisterController::class)->group(function()
+{
+    Route::post('register','register');
+    Route::post('login','login');
+});
